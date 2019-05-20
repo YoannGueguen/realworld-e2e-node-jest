6d342bf886c5d09fe0a07515cbefb7eee6330d7d
KevSib <ksibue@gmail.com>
2019-05-20 13:13:50 +0200

    finalize tests

--- src/realworld.test.js
+++ src/realworld.test.js
@@ -30,12 +30,30 @@ test('create article with connected user', async () => {
     await createArticle(testArticleTitle, testArticleAbout, testArticleContent, testArticleTags);
 
     //  see header show with title equal to previous enter title
-    await driver.wait(until.elementLocated(By.css('div.article-page')), 4000);
-
     const articlePage = await driver.findElement(By.css('div.article-page'));
     expect(articlePage).toBePresent();
 
 });
+
+test('create article and delete it', async () => {
+
+    await driver.get('http://localhost:4100/');
+
+    //  Connect user first
+    await connectUserIfNeeded(EMAIL, PASSWORD, USERNAME);
+    
+    await createArticle( "coucou","coucou","coucou","coucou");
+
+    const removeArticleBtn = await driver.findElement(By.css('button[class="btn btn-outline-danger btn-sm"]'));
+    removeArticleBtn.click();
+
+    await driver.wait(until.elementLocated(By.css('div.article-preview')), 4000);
+
+    const yourFeedBnt = await driver.findElement(By.partialLinkText('Your Feed'));
+
+    expect(yourFeedBnt).toBePresent();
+
+ });
 
 test('register user', async () => {
 
@@ -54,104 +72,100 @@ test('register user', async () => {
 })
 
 async function createArticle(testArticleTitle, testArticleAbout, testArticleContent, testArticleTags) {
+    
     await driver.wait(until.elementLocated(By.partialLinkText('New Post')), 4000);
+
     //  click on button 'new post's
     const newPostLink = await driver.findElement(By.linkText('New Post'));
     await newPostLink.click();
+
     //  fill article title
     const articleTitleField = await driver.findElement(By.css('input[placeholder="Article Title"]'));
     articleTitleField.sendKeys(testArticleTitle);
+
     //  fill about field
     const aboutField = await driver.findElement(By.css('input[placeholder="What\'s this article about?"]'));
     aboutField.sendKeys(testArticleAbout);
+
     //  fill article content markdown
     const contentField = await driver.findElement(By.css('textarea[placeholder="Write your article (in markdown)"]'));
     contentField.sendKeys(testArticleContent);
+
     //  fill tags
     const tagsField = await driver.findElement(By.css('input[placeholder="Enter tags"]'));
     tagsField.sendKeys(testArticleTags);
+
     //  press enter to validates
     tagsField.sendKeys("\n");
+
     //  click on  publish article
     const publishButton = await driver.findElement(By.css('button[type="button"]'));
-
     publishButton.click();
+
+    await driver.wait(until.elementLocated(By.css('div.article-page')), 4000);
+
 }
-
- test('create article and delete it', async () => {
-    await driver.get('http://localhost:4100/');
-    
-    await createArticle( "coucou","coucou","coucou","coucou");
-
-    const removeArticleBtn = await driver.findElement(By.css('button[class="btn btn-outline-danger btn-sm"]'));
-    expect(removeArticleBtn).toBePresent();
-    removeArticleBtn.click();
- });
 
 async function registerUser(username, email, pwd){
 
     const signUpLink = await driver.findElement(By.linkText('Sign up'));   
-    expect(signUpLink).toBePresent(); 
     await signUpLink.click();
 
     await driver.wait(until.elementLocated(By.css('input[placeholder="Username"]')), 4000);
 
     const usernameField = await driver.findElement(By.css('input[placeholder="Username"]'));
-    expect(usernameField).toBePresent();
     usernameField.sendKeys(username);
 
     const emailField = await driver.findElement(By.css('input[placeholder="Email"]'));
-    expect(emailField).toBePresent();
     emailField.sendKeys(email);
 
     const password = await driver.findElement(By.css('input[placeholder="Password"]'));
-    expect(password).toBePresent();
     password.sendKeys(pwd);
 
     const signUpButton = await driver.findElement(By.css('button[type="submit"]'));
-    expect(signUpButton).toBePresent();
     signUpButton.click();
 
     await driver.wait(until.elementLocated(By.linkText('Home')), 4000);
 
 }
 
-test('Like article if it exists', async () => {
+test('like article if it exists then create article', async () => {
 
     await driver.get('http://localhost:4100');
 
     await connectUserIfNeeded(EMAIL, PASSWORD, USERNAME);
 
-    const beforeClickVal = await likesArticles();
+    await likesArticlesIfExists();
 
-    const ionHearts = await driver.findElements(By.xpath("//button[contains(@class, 'btn btn-sm btn-outline-primary')]"));
-    const firstOne = ionHearts[0];
-    const afterClick = await firstOne.getText();
+    const rnd = getRandomInt(10000);
+    const fakeVal = "fakeArticle" + rnd;
+    await createArticle(
+        fakeVal,
+        fakeVal, 
+        fakeVal, 
+        fakeVal
+    );
 
-    console.log('before', beforeClickVal);
-    console.log('after', afterClick);
+    await homeClick();
+
+    await globalFeedClick();
+
+    const newArticleTitle = await driver.findElement(By.xpath("//h1[text() = '" + fakeVal + "']"));
+
+    expect(newArticleTitle).toBePresent();
 
 });
 
-async function likesArticles() {
+async function likesArticlesIfExists() {
 
-    //  wait page
-    await driver.wait(until.elementLocated(By.partialLinkText('Global Feed')), 4000);
-
-    //  go to global feed
-    const globalFeedBnt = await driver.findElement(By.partialLinkText('Global Feed'));
-    await globalFeedBnt.click();
-    
-    //  wait sub-page
-    await driver.wait(until.elementLocated(By.css('div.article-preview')), 4000);
+    await globalFeedClick();
 
     //  click on fav button
     
     const ionHearts = await driver.findElements(By.xpath("//button[contains(@class, 'btn btn-sm btn-outline-primary')]"));
-    const beforeClick = await ionHearts[0].getText();
-    await ionHearts[0].click();
-
-    return beforeClick;
+    if (ionHearts.length > 0) {
+        await ionHearts[0].click();
+    }
 
 }
 
@@ -161,7 +175,7 @@ async function likesArticles() {
 
 async function connectUserIfNeeded(email, password, username) {
     
-    await homeClick(username);
+    await homeClick();
 
     const signInLink = await driver.findElement(By.linkText('Sign in'));
 
@@ -197,8 +211,22 @@ async function connectUser(email, mdp) {
 
 }
 
-async function homeClick(username) {
-    const homeBtn = await driver.findElement(By.linkText(username));
+async function globalFeedClick() {
+
+    //  wait page
+    await driver.wait(until.elementLocated(By.partialLinkText('Global Feed')), 4000);
+
+    //  go to global feed
+    const globalFeedBnt = await driver.findElement(By.partialLinkText('Global Feed'));
+    await globalFeedBnt.click();
+
+    //  wait sub-page
+    await driver.wait(until.elementLocated(By.css('div.article-preview')), 4000);
+
+}
+
+async function homeClick() {
+    const homeBtn = await driver.findElement(By.linkText("Home"));
     homeBtn.click();
 }
 
